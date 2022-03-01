441d4f7b231db7b42d5a3103205b5e8a8767df72
KranthiDevtest <98391432+KranthiDevtest@users.noreply.github.com>
2022-03-01 12:38:31 +0530

    updated ami id

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-020db2c14939a8efb" 
+    default     = "ami-0ae12517fe595ce17" 
 }
 variable "versioning" {
     type        = bool
