7c4a7da347c1367c003325c75fe16cd11bc20c24
KranthiDevtest <98391432+KranthiDevtest@users.noreply.github.com>
2022-03-01 11:57:25 +0530

    updated

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  acl = "private"
+  acl = private
   
    versioning {
-    enabled = "true"
+    enabled = true
   }
   
   tags = var.tags
